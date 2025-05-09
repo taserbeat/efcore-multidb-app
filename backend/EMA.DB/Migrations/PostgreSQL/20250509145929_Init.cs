ce7b8379aeb5613e411f0ed8123288bd1c6a004e
taserbeat <taserbeat@gmail.com>
2025-05-10 00:02:24 +0900

    chore: PostgreSQLとSQLServerのマイグレーソンファイルを作成

---
+++ backend/EMA.DB/Migrations/PostgreSQL/20250509145929_Init.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMA.DB.Migrations.PostgreSQL
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    product_id = table.Column<Guid>(type: "uuid", nullable: false, comment: "商品のID"),
+                    name = table.Column<string>(type: "text", nullable: false, comment: "商品の名前")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.product_id);
+                },
+                comment: "商品のテーブル");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
---
+++ backend/EMA.DB/Migrations/SQLServer/20250509150002_Init.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EMA.DB.Migrations.SQLServer
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "products",
+                columns: table => new
+                {
+                    product_id = table.Column<Guid>(type: "uniqueidentifier", nullable: false, comment: "商品のID"),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: false, comment: "商品の名前")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_products", x => x.product_id);
+                },
+                comment: "商品のテーブル");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "products");
+        }
+    }
+}
